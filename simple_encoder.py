56e677c3ecacf53f3ce8dca5265bcbff316cb45e
aahanjandir <aahanjandir1@gmail.com>
2023-10-25 12:31:17 -0400

    first commit

---
+++ simple_encoder.py
@@ -0,0 +1,28 @@
+# Aahan Jandir
+
+def menu():
+    print('MENU')
+    print(13* '-')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+
+def encode(num):
+    num_list = list(num)
+    for i in range(len(num_list)):
+        num_list[i] = str(int(num[i]) + 3)
+    return ''.join(num_list)
+
+def main(): 
+    x = True
+    while x == True:
+        menu()
+        user_option = input('Please enter an option: ')
+        if user_option == '1':
+            user_num = (input('Please enter your password to encode: '))
+            encode(user_num)
+            print('Your password has been encoded and stored!')
+        if user_option == '3':
+            x = False
+    
+main()
